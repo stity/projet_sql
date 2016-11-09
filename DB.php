a6a1d3d6518f06d2b9325d9f0be8bd75869142ab
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-11-09 09:01:39 +0100

    add : php class

---
+++ DB.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class DB {
+    var $servername;
+    var $username;
+    var $password;
+    var $dbname;
+    var $conn;
+
+    function __construct()  {
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "";
+        $this->dbname = "telephonie";
+
+        // Create connection
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    function close () {
+        $this->conn->close();
+    }
+}
+
+var $db = new DB();
+$db->close();
+
+?>
